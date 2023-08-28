46770791a725acb8358b2790ebd1516354ac7ddb
Patiencemugambi <patiencemugambi@gmail.com>
2023-08-28 20:18:34 +0300

    customer.py

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # PythonWk2
+# Object Relations Code Challenge - Restaurants
+
+For this project, we'll be working with a Yelp-style domain.
+
+
+License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+Author's Info
+
+- LinkedIn: [Patience Mugambi](https://www.linkedin.com/feed/)
---
+++ customer.py
@@ -0,0 +1,38 @@
+class Customer:
+    _all_customers = []  # variable to store all customer instances
+
+    def __init__(self, given_name, family_name):
+        self._given_name = given_name
+        self._family_name = family_name
+        Customer._all_customers.append(self)  # Add the instance to the list of all customers
+
+    # Methods for accessing and modifying the given_name
+    def given_name(self):
+        return self._given_name
+
+    def set_given_name(self, new_given_name):
+        self._given_name = new_given_name
+
+    # Methods for family name
+    def family_name(self):
+        return self._family_name
+
+    def set_family_name(self, new_family_name):
+        self._family_name = new_family_name
+
+    # Method for getting the full name of the customer
+    def full_name(self):
+        return f"{self._given_name} {self._family_name}"
+
+    # method to retrieve all customer instances
+    @classmethod
+    def all(cls):
+        return cls._all_customers
+
+# Test the Customer class
+customer1 = Customer('Patience', 'Mugambi')
+customer2 = Customer('Alice', 'Minaj')
+
+print(customer1.full_name())  # Output: John Doe
+print(customer2.full_name())  # Output: Jane Smith
+print(Customer.all())  # Output: [customer1, customer2]
